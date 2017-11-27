25545f5ab7ed4ccf7c9477641699216dfb5a3de0
Marcin Markowski <31286082+technites-pl@users.noreply.github.com>
2017-11-27 11:04:09 +0100

    Aggregated Message and ToString in AggregateAssertException
    (cherry picked from commit 6346baf)

---
+++ ITLibrium.BDD.Tests/Scenarios/AggregateAssertExceptionTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using ITLibrium.Bdd.Scenarios;
+using Shouldly;
+using Xunit;
+
+namespace ITLibrium.BDD.Tests.Scenarios
+{
+    public class AggregateAssertExceptionTests
+    {
+        [Fact]
+        public void MessageEqualsSingleExceptionMessage()
+        {
+            var exception = new Exception("Custom message");
+            var aggregateException = new AggregateAssertException(new[]{exception});
+            aggregateException.Message.ShouldBe(exception.Message);
+        }
+        
+        [Fact]
+        public void ToStringEqualsSingleExceptionToString()
+        {
+            var exception = new Exception("Custom message");
+            var aggregateException = new AggregateAssertException(new[]{exception});
+            aggregateException.ToString().ShouldBe(exception.ToString());
+        }
+        
+        [Fact]
+        public void MessageIsAggregatedIfMoreThanOneException()
+        {
+            var exception1 = new Exception("Custom message 1");
+            var exception2 = new Exception("Custom message 2");
+            var aggregateException = new AggregateAssertException(new[]{exception1, exception2});
+            aggregateException.Message.ShouldBe(
+                $"More than one assert failed.{Environment.NewLine}" +
+                $"{Environment.NewLine}" +
+                $"1) {exception1.Message}{Environment.NewLine}" +
+                $"{Environment.NewLine}" +
+                $"2) {exception2.Message}{Environment.NewLine}");
+        }
+        
+        [Fact]
+        public void ToStringIsAggregatedIfMoreThanOneException()
+        {
+            var exception1 = new Exception("Custom message 1");
+            var exception2 = new Exception("Custom message 2");
+            var aggregateException = new AggregateAssertException(new[]{exception1, exception2});
+            aggregateException.ToString().ShouldBe(
+                $"More than one assert failed.{Environment.NewLine}" +
+                $"{Environment.NewLine}" +
+                $"1) {exception1}{Environment.NewLine}" +
+                $"{Environment.NewLine}" +
+                $"2) {exception2}{Environment.NewLine}");
+        }
+    }
+}
--- ITLibrium.BDD/Scenarios/AggregateAssertException.cs
+++ ITLibrium.BDD/Scenarios/AggregateAssertException.cs
@@ -7,20 +7,44 @@ namespace ITLibrium.Bdd.Scenarios
 {
     public class AggregateAssertException : Exception
     {
-        private readonly Exception[] _exceptions;
+        private readonly IReadOnlyList<Exception> _exceptions;
 
+        public override string Message => GetAggregatedInfo(e => e.Message);
+        
         public AggregateAssertException(IEnumerable<Exception> exceptions)
         {
-            _exceptions = exceptions.ToArray();
+            IReadOnlyList<Exception> exceptionsList = exceptions.ToList();
+            if(exceptionsList.Count == 0)
+                throw new ArgumentException("Exceptions collection can not be empty.", nameof(exceptions));
+            
+            _exceptions = exceptionsList;
         }
 
         public override string ToString()
         {
+            return GetAggregatedInfo(e => e.ToString());
+        }
+
+        private string GetAggregatedInfo(Func<Exception, string> infoFactory)
+        {
+            int exceptionsCount = _exceptions.Count;
+            if (exceptionsCount == 1)
+                return infoFactory(_exceptions[0]);
+            
             var builder = new StringBuilder();
-
-            foreach (Exception exception in _exceptions)
-                builder.AppendLine(exception.ToString());
-
+            builder.AppendLine("More than one assert failed.");
+            builder.AppendLine();
+            for (var i = 0; i < exceptionsCount; i++)
+            {
+                Exception exception = _exceptions[i];
+                
+                builder.Append($"{i + 1}) ");
+                
+                string info = infoFactory(exception);
+                builder.AppendLine(info);
+                if (!info.EndsWith(Environment.NewLine) && i != exceptionsCount - 1)
+                    builder.AppendLine();
+            }
             return builder.ToString();
         }
     }
